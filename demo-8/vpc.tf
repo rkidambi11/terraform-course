3db9b98c9dbe5824fc6d56889e2237b9a2c389cc
Ranga_Kidambi <40720902+rkidambi11@users.noreply.github.com>
2019-05-27 21:44:41 +0530

    added ap-south-1a and 1b zone and removed 1c

--- demo-8/vpc.tf
+++ demo-8/vpc.tf
@@ -16,7 +16,7 @@ resource "aws_subnet" "main-public-1" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.1.0/24"
     map_public_ip_on_launch = "true"
-    availability_zone = "eu-west-1a"
+    availability_zone = "ap-south-1a"
 
     tags {
         Name = "main-public-1"
@@ -26,27 +26,18 @@ resource "aws_subnet" "main-public-2" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.2.0/24"
     map_public_ip_on_launch = "true"
-    availability_zone = "eu-west-1b"
+    availability_zone = "ap-south-1b"
 
     tags {
         Name = "main-public-2"
     }
 }
-resource "aws_subnet" "main-public-3" {
-    vpc_id = "${aws_vpc.main.id}"
-    cidr_block = "10.0.3.0/24"
-    map_public_ip_on_launch = "true"
-    availability_zone = "eu-west-1c"
 
-    tags {
-        Name = "main-public-3"
-    }
-}
 resource "aws_subnet" "main-private-1" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.4.0/24"
     map_public_ip_on_launch = "false"
-    availability_zone = "eu-west-1a"
+    availability_zone = "ap-south-1a"
 
     tags {
         Name = "main-private-1"
@@ -56,20 +47,10 @@ resource "aws_subnet" "main-private-2" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.5.0/24"
     map_public_ip_on_launch = "false"
-    availability_zone = "eu-west-1b"
+    availability_zone = "ap-south-1a"
 
     tags {
         Name = "main-private-2"
-    }
-}
-resource "aws_subnet" "main-private-3" {
-    vpc_id = "${aws_vpc.main.id}"
-    cidr_block = "10.0.6.0/24"
-    map_public_ip_on_launch = "false"
-    availability_zone = "eu-west-1c"
-
-    tags {
-        Name = "main-private-3"
     }
 }
 
@@ -102,9 +83,5 @@ resource "aws_route_table_association" "main-public-1-a" {
 }
 resource "aws_route_table_association" "main-public-2-a" {
     subnet_id = "${aws_subnet.main-public-2.id}"
-    route_table_id = "${aws_route_table.main-public.id}"
-}
-resource "aws_route_table_association" "main-public-3-a" {
-    subnet_id = "${aws_subnet.main-public-3.id}"
     route_table_id = "${aws_route_table.main-public.id}"
 }
